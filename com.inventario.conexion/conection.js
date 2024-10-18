f8af757d63c05fdaee3a56b2fdc600059057804a
RJDP <reinaldo0602@gmail.com>
2024-10-18 16:10:01 -0500

    modificando conection.js

---
+++ com.inventario.conexion/conection.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+import { createConnection } from 'mysql2';
+
+const connection = createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// Conectar a la base de datos
+connection.connect((error) => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        return;
+    }
+    console.log('Conexión a la base de datos MySQL exitosa');
+});
+
+export default connection;
